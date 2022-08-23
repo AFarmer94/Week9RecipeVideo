71d57e638a2f03e382653bea814d08e0235d9e40
Andrew Farmer <thefarmer94@gmail.com>
2022-08-22 21:17:56 -0500

    Problems

--- src/main/java/recipes/exception/DbException.java
+++ src/main/java/recipes/exception/DbException.java
@@ -1,20 +1,25 @@
-package projects.exception;
+package recipes.exception;
 
+@SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
 	public DbException() {
-		
+
 	}
 
 	public DbException(String message) {
+		super(message);
 		
 	}
 
 	public DbException(Throwable cause) {
-		
+		super(cause);
+	
 	}
 
 	public DbException(String message, Throwable cause) {
-		
+		super(message, cause);
+
 	}
+
 }
